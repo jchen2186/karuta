e8cf779dead09ab85f00a8212459c1860575fe32
Jane Chen <jane.chen@macaulay.cuny.edu>
2018-12-27 00:23:18 -0500

    Abstracted code, added tiles to help divided poems into columns

--- js/main.js
+++ js/main.js
@@ -14,64 +14,26 @@ files.forEach(function(url) {
 });
 
 Promise.all(promises).then(function(values) {
-	var jpPoems = values[0];
-	var romajiPoems = values[1];
-	var engPoems = values[2];
-
-	var data = preparePoems(jpPoems, romajiPoems, engPoems);
-	jpPoems = data[0];
-	romajiPoems = data[1];
-	engPoems = data[2];
-
-	// createTiles(jpPoems, romajiPoems, engPoems);
 	createTiles(values);
-
 })
 
-function preparePoems(jpPoems, romajiPoems, engPoems) {
-	let jpData = jpPoems.map(row => {
-		return [row['poet'],
-						row['line1'] + '<br>'
-						+ row['line2'] + '<br>'
-						+ row['line3'] + '<br>'
-						+ row['line4'] + '<br>'
-						+ row['line5']]
-	});
+function createTiles(data, activeTab = "jp") {
+	let index = ["jp", "romaji", "eng"].indexOf(activeTab);
+	let filteredData = data[index];
 
-	let romajiData = romajiPoems.map(row => {
-		return [row['poet'],
-						row['line1'] + '<br>'
-						+ row['line2'] + '<br>'
-						+ row['line3'] + '<br>'
-						+ row['line4'] + '<br>'
-						+ row['line5']]
-	});
-
-	let engData = engPoems.map(row => {
-		return [row['poet'],
-						row['line1'] + '<br>'
-						+ row['line2'] + '<br>'
-						+ row['line3'] + '<br>'
-						+ row['line4'] + '<br>'
-						+ row['line5']]
-	});
-
-	let data = [jpData, romajiData, engData];
-	return data;
+	fillColumn(filteredData.slice(0,33), 1);
+	fillColumn(filteredData.slice(33,66), 2);
+	fillColumn(filteredData.slice(66), 3);
 }
 
-// function createTiles(jpPoems, romajiPoems, engPoems) {
-function createTiles(data) {
-	let col1 = d3.select("#col1");
-	let col2 = d3.select("#col2");
-	let col3 = d3.select("#col3");
+function fillColumn(data, columnNumber) {
+	var col = d3.select(`#col${columnNumber}`);
 
-	let cards = col1.selectAll(".card")
-	// .data(jpPoems)
-		.data(data[0])
+	let cards = col.selectAll(".card")
+		.data(data)
 		.enter()
 		.append("div")
-			.attr("class", "card");
+			.attr("class", "tile is-child card has-text-centered");
 	
 	let tabs = cards.append("div")
 	.attr("class", "tabs is-fullwidth is-boxed")
@@ -104,89 +66,4 @@ function createTiles(data) {
 							+ d['line3'] + '<br>'
 							+ d['line4'] + '<br>'
 							+ d['line5']);
-
-	
-
-	// let card = col1.append("div")
-	// 	.attr("class", "card");
-
-	// let tabs = card.append("div")
-	// 	.attr("class", "tabs is-fullwidth is-boxed")
-	// 	.append("ul");
-
-	// tabs.append("li")
-	// 	.attr("class", "is-active")
-	// 	.append("a")
-	// 		.text("Japanese");
-	
-	// tabs.append("li")
-	// 	.append("a")
-	// 		.text("Romaji");
-	
-	// tabs.append("li")
-	// 	.append("a")
-	// 		.text("English");
-
-	// let cardContent = card.append("div")
-	// 	.attr("class", "card-content");
-			
-	// cardContent.append("h4")
-	// 	.attr("class", "title is-4")
-	// 	.text("Poet Name");
-	
-	// cardContent.append("div")
-	// 	.attr("class", "content")
-	// 	.text("Poem");
-
-	console.log(jpPoems);
 }
-
-
-
-// col1.append("div")
-// .attr("class", "card")
-// .append("div")
-// .attr("class", "card-content")
-// .append("div")
-// .attr("class", "content")
-// .text();
-
-// col1.append("div")
-// .attr("class", "card")
-// .append("div")
-// .attr("class", "card-content")
-// .append("div")
-// .attr("class", "content")
-// .text("test");
-
-// col2.append("div")
-// .attr("class", "card")
-// .append("div")
-// .attr("class", "card-content")
-// .append("div")
-// .attr("class", "content")
-// .text("test");
-
-// col2.append("div")
-// .attr("class", "card")
-// .append("div")
-// .attr("class", "card-content")
-// .append("div")
-// .attr("class", "content")
-// .text("test");
-
-// col3.append("div")
-// .attr("class", "card")
-// .append("div")
-// .attr("class", "card-content")
-// .append("div")
-// .attr("class", "content")
-// .text("test");
-
-// col3.append("div")
-// .attr("class", "card")
-// .append("div")
-// .attr("class", "card-content")
-// .append("div")
-// .attr("class", "content")
-// .text("test");
